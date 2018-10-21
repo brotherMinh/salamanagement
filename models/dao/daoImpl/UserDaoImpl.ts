11bac7bd4a1ac67c02a6a29d318d30c3142c40c0
Nguyễn Trường Sơn <sonnt6789p@gmail.com>
2018-10-21 19:16:57 +0700

    Login - UserDaoImpl

--- models/dao/daoImpl/UserDaoImpl.ts
+++ models/dao/daoImpl/UserDaoImpl.ts
@@ -1,17 +1,30 @@
 ///<reference path="../UserDao.ts"/>
 import { BaseDaoImpl } from "./BaseDaoImpl";
+var jssha = require('js-sha512');
 
+const QUERY_GET_SALT = `SELECT SALT FROM USER U WHERE U.USERNAME = ?;`;
 const QUERY_CHECK_AUTH_LOGIN = `SELECT * FROM USER U WHERE U.USERNAME = ? AND U.PASSWORD = ?;`;
 
 export class UserDaoImpl extends BaseDaoImpl implements UserDao {
   getUserByUserName(userName: String, password: String) {
+    var salt = '';
+
     const con = this.getConnection("localhost", "root", "root", "sale_management");
     con.connect(err => {
       if (err) {
         console.log(err);
       }
     });
-    con.query(QUERY_CHECK_AUTH_LOGIN, [userName, password], (err, result) => {
+    //get salt
+    con.query(QUERY_GET_SALT, [userName], (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      salt = result;
+    });
+
+    //check user
+    con.query(QUERY_CHECK_AUTH_LOGIN, [userName, jssha.sha512.array(password+salt)], (err, result) => {
         if (err) {
             console.log(err);
         }
